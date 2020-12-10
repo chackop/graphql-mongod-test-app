05654f9bbd17911ecf69d97d00a9a0bfe2e09db8
ChxDesk <chackop@gmail.com>
2020-12-10 17:24:45 +0000

    added basic graphql repsonse

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import express from 'express';
+import express from "express";
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Graphql setup');
+app.get("/", (req, res) => {
+  res.send("Graphql setup");
 });
 
-app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
+const root = { hello: () => "Hello request for graphql" };
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(8080, () =>
+  console.log("Running server on port localhost:8080/graphql")
+);
