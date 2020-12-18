503d0f00d894330edaad1adad8a6eb3fc6715168
ChxDesk <chackop@gmail.com>
2020-12-18 18:51:50 +0000

    added dbconnectors for mongo and mysql

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
-import schema from "./schema";
-import resolvers from "./resolvers";
 import { graphqlHTTP } from "express-graphql";
+import { schema } from "./data/schema";
 
 const app = express();
 
@@ -9,17 +8,14 @@ app.get("/", (req, res) => {
   res.send("Graphql setup");
 });
 
-const root = resolvers;
-
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
 
 app.listen(8080, () =>
-  console.log("Running server on port localhost:8080/graphql")
+  console.log("Running on server port localhost:8080/graphql")
 );
